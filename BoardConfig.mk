879c70b1dd07e54435e122c315110dea8af093de
SamarV-121 <samarvispute121@gmail.com>
2021-01-27 08:55:34 +0530

    A5_Pro: Use patchelf instead of LD_SHIM_LIBS
    Change-Id: I7bf8e64de773e88ca6e7ddabd85571afdd857c28
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: wulan17 <galihgustip@gmail.com>
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,10 +108,6 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 # Releasetools
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
-
-# Shims
-TARGET_LD_SHIM_LIBS := \
-    /system/lib/libshowlogo.so|libshim_showlogo.so
     
 # Treble
 BOARD_VNDK_VERSION := current
