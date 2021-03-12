c965661ed8320eeac50d7e0970fc60218069aefb
Hadenix <vsegda_online_007@bk.ru>
2021-03-12 14:23:43 +0300

    A5_Pro: Generate privapp-permissions for mediatek IMS
    Reference:[0]: https://source.android.com/devices/tech/config/perms-whitelist

--- device.mk
+++ device.mk
@@ -11,6 +11,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml    
+
 # APN
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/apns/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
