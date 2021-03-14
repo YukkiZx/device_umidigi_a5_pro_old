1fcc79e1f3bf93c44f81a1196e248cbcccd3018e
jmpfbmx <jmpf.bmx@gmail.com>
2021-03-14 11:03:56 +0100

    A5_Pro: Define TARGET_INIT_VENDOR_LIB correctly
     * We are using DEVICE_PATH not LOCAL_PATH
    
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,7 +117,7 @@ TARGET_LD_SHIM_LIBS := \
 BOARD_VNDK_VERSION := current
 
 # Board init
-TARGET_INIT_VENDOR_LIB := //$(LOCAL_PATH):libinit_A5_Pro
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_A5_Pro
 TARGET_RECOVERY_DEVICE_MODULES := libinit_A5_Pro
 
 # Soong namespaces
