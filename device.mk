34a890a9ddb41f5c5445affba198e11710dada61
Hadenix <vsegda_online_007@bk.ru>
2021-03-28 18:20:32 +0300

    A5_Pro: correct text description

--- device.mk
+++ device.mk
@@ -85,7 +85,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.secure_lock_screen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.secure_lock_screen.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
         
-# RCS
+# RcsService
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
     PresencePolling \
