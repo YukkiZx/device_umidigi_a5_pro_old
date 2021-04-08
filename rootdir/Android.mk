532fc5010d7134852a0a39e0c04694429a564005
Hadenix <vsegda_online_007@bk.ru>
2021-04-08 21:21:50 +0300

    A5_Pro: zram: set the correct path
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -19,7 +19,7 @@ LOCAL_MODULE       := fstab.enableswap
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.enableswap
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
