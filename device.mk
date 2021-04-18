24a8acdf692f9bcae08405c66207c4e32ba67fe8
Hadenix <vsegda_online_007@bk.ru>
2021-04-18 15:20:45 +0300

    A5_Pro: The list of components which should be automatically disabled for a specific device
    Signed-off-by: Hadenix <vsegda_online_007@bk.ru>

--- device.mk
+++ device.mk
@@ -10,7 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
     
 # APNs
 PRODUCT_COPY_FILES += \
